6d2351d051cbd20a4caa8dd301f43a0fecb76bb3
Jeff Buchbinder <jeff@freemedsoftware.org>
2010-07-30 16:22:30 +0000

    Bump version number to 0.6 for tracking. Push input path as parameter to reports so sub reports can resolve paths as "ipath". Change dependency on jasperreports from 3.7.3 -> 3.7.4

--- src/main/java/org/freemedsoftware/util/JasperWrapper.java
+++ src/main/java/org/freemedsoftware/util/JasperWrapper.java
@@ -33,7 +33,7 @@ import net.sf.jasperreports.engine.xml.JRXmlLoader;
 
 public class JasperWrapper {
 
-	public static String VERSION = "0.5";
+	public static String VERSION = "0.6";
 
 	private static Hashtable<String, String> arguments = new Hashtable<String, String>();
 
@@ -137,6 +137,9 @@ public class JasperWrapper {
 									+ "] " + reportParameters.get(iter));
 						}
 					}
+
+					// Push input path into report parameters
+					hm.put("ipath", arguments.get("ipath"));
 
 					// Fill out report
 					System.err.println("Filling report");
